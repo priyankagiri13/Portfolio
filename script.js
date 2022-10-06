44ed4f71d8b98eb2fdce1742a960488cbf74fc54
priyankagiri13 <111486961+priyankagiri13@users.noreply.github.com>
2022-10-06 14:21:37 +0530

    first commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function slideCircle(){
         .forEach(function(slide){
             slide.addEventListener("mousemove", function(dets){
                 var dim = slide.getBoundingClientRect();
-                this.children[1].style.clipPath = `circle(20% at ${dets.clientX-dim.left}px ${dets.clientY-dim.top}px)`;
+                this.children[1].style.clipPath = `circle(18% at ${dets.clientX-dim.left}px ${dets.clientY-dim.top}px)`;
             });
 
             slide.addEventListener("mouseleave", function(dets){
